4525e4966f5b643a0a8c733bdfcd36bf263a4138
bay-puz <68962000+bay-puz@users.noreply.github.com>
2020-07-31 02:03:18 +0900

    カギの数字をニコリ式に取る

--- crossword/crossword.py
+++ crossword/crossword.py
@@ -2,31 +2,38 @@
 import argparse
 
 
-def load_file(file: str) -> str:
+def load_file(file: str) -> list:
     fopen = open(file)
     input_lines = fopen.read().splitlines()
 
     return input_lines
 
 
-def cut_line(lines: list) -> dict:
-    num = 1
-    words = {}
-    for line in lines:
-        word = ""
-        for chara in line:
-            if chara in  ('＠', '@', '＃', '#'):
-                if len(word) > 1:
-                    words[num] = word
-                    num += 1
-                word = ''
-            else:
-                word += chara
-        if len(word) > 1:
-            words[num] = word
-            num += 1
+def is_black(chara: str) -> bool:
+    return chara in ('@', '＠', '#', '＃')
 
-    return words
+
+def is_number(row: int, column: int, board: list) -> bool:
+    row_end = len(board[0])
+    column_end = len(board)
+
+    def is_whitecell(row: int, column: int) -> bool:
+        if row not in range(0, row_end):
+            return False
+        if column not in range(0, column_end):
+            return False
+
+        return not is_black(board[column][row])
+
+    if not is_whitecell(row, column):
+        return False
+
+    if not is_whitecell(row-1, column) and is_whitecell(row+1, column):
+        return True
+    if not is_whitecell(row, column-1) and is_whitecell(row, column+1):
+        return True
+
+    return False
 
 
 def transposition_list(lines: list) -> list:
@@ -38,7 +45,60 @@ def transposition_list(lines: list) -> list:
             else:
                 tr_dict[column] += chara
 
-    return tr_dict.values()
+    return list(tr_dict.values())
+
+
+def set_keys(lines: str) -> dict:
+    lines_tr = transposition_list(lines)
+
+    keys = {}
+    keys_tr = {}
+    key_id = 1
+    for column, line in enumerate(lines_tr):
+        for row, _ in enumerate(line):
+            if is_number(row, column, lines_tr):
+                keys[key_id] = [column, row]
+                keys_tr[key_id] = [row, column]
+                key_id += 1
+
+    return keys, keys_tr
+
+
+def set_words(lines: dict, keys: dict) -> dict:
+    words = {}
+    head = 0
+    for line in lines:
+        word = ''
+        for chara in line:
+            if is_black(chara):
+                if len(word) > 1:
+                    words[head] = word
+                head += len(word) + 1
+                word = ''
+            else:
+                word += chara
+        if len(word) > 1:
+            words[head] = word
+        head += len(word)
+
+    row_size = len(lines[0])
+    words_key = {}
+    for key_id, key_pos in keys.items():
+        pos = key_pos[0] + key_pos[1] * row_size
+        if pos in words:
+            words_key[key_id] = words[pos]
+
+    return words_key
+
+
+def get_words_keys(lines: list):
+    lines_tr = transposition_list(lines)
+    keys, keys_tr = set_keys(lines)
+
+    row_words = set_words(lines, keys)
+    column_words = set_words(lines_tr, keys_tr)
+
+    return row_words, column_words
 
 
 def load_problem(file: str) -> dict:
@@ -47,11 +107,9 @@ def load_problem(file: str) -> dict:
     problem = {}
     problem["board"] = board
 
-    row_words = cut_line(board)
-    problem["row"] = row_words
-
-    column_words = cut_line(transposition_list(board))
-    problem["column"] = column_words
+    row_words, column_words = get_words_keys(board)
+    problem["row"] = sorted(row_words.items())
+    problem["column"] = sorted(column_words.items())
 
     return problem
 
@@ -64,10 +122,10 @@ def show_problem(problem: dict) -> None:
         print(line)
     print()
 
-    keys = {"row": "↓ヨコのカギ", "column": "↓タテのカギ"}
+    keys = {"row": "→ヨコのカギ", "column": "↓タテのカギ"}
     for key, str_key in keys.items():
         print(str_key)
-        for num, word in problem[key].items():
+        for num, word in problem[key]:
             print("{} {}".format(num, word))
         print()
 
