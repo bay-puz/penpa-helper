8a456530a572b081e5168b8300fbd1c06e41daaf
bay-puz <68962000+bay-puz@users.noreply.github.com>
2021-05-18 23:14:02 +0900

    灰色マスに対応

--- selectwords/selectwords.py
+++ selectwords/selectwords.py
@@ -2,7 +2,9 @@
 import argparse
 from re import fullmatch
 import itertools
-from numpy import sort
+
+
+GRAY = '🔲'
 
 
 def load_jisho(file: str, width: int = -1) -> list:
@@ -21,7 +23,9 @@ def load_jisho(file: str, width: int = -1) -> list:
 
 def load_problem(file: str) -> list:
     fopen = open(file)
-    problem = fopen.read().splitlines()
+    input_problem = fopen.read().splitlines()
+
+    problem = [word.replace('#', GRAY).replace('＃', GRAY) for word in input_problem]
 
     def _is_valid() -> bool:
         if len(problem) < 2 or len(problem[0]) < 2:
@@ -35,53 +39,124 @@ def load_problem(file: str) -> list:
     return problem if _is_valid() else []
 
 
-def get_candidates(jisho: list, problem: list) -> list:
+def find_candidates(jisho: list, problem: list) -> list:
+    patterns = generate_patterns(problem)
+
     candidates = []
-    width = len(problem[0])
-    height = len(problem)
-
-    pattern = ''
-    for row in range(width):
-        characters = ''
-        for column in range(height):
-            characters += problem[column][row]
-        pattern = pattern + '[' + characters + ']'
-
     for word in jisho:
-        if fullmatch(pattern, word):
-            candidates.append(word)
+        for pattern, pos_gray in patterns:
+            if fullmatch(pattern, word):
+                gray_char = word[pos_gray] if pos_gray >= 0 else None
+                candidates.append((word, gray_char))
+                break
 
     return candidates
 
 
+def generate_patterns(problem: list) -> list:
+    width = len(problem[0])
+    height = len(problem)
+
+    count_gray = [0 for _ in range(width)]
+    pattern_char = [None for _ in range(width)]
+    for row in range(width):
+        characters = ''
+        for column in range(height):
+            if problem[column][row] == GRAY:
+                count_gray[row] += 1
+            else:
+                characters += problem[column][row]
+        if characters != '':
+            pattern_char[row] = '[' + characters + ']'
+
+    def _check_pattern(pattern_set: list) -> bool:
+        for row in range(width):
+            if pattern_set[row]:
+                if not pattern_char[row]:
+                    return False
+            else:
+                if count_gray[row] == 0:
+                    return False
+        return True
+
+    patterns = []
+    for pattern_set in itertools.product([True, False], repeat=width):
+        if not _check_pattern(pattern_set):
+            continue
+
+        pattern = ''
+        pos_gray = -1
+        for row in range(width):
+            if pattern_set[row]:
+                pattern += pattern_char[row]
+            else:
+                if pos_gray == -1:
+                    pattern += '(.)'
+                    pos_gray = row
+                else:
+                    pattern += r'\1'
+        patterns.append((pattern, pos_gray))
+
+    return patterns
+
+
 def solve(jisho: list, problem: list) -> list:
-    sorted_problem = sort_selectwords(problem)
-    candidates = get_candidates(jisho, problem)
+    candidates = find_candidates(jisho, problem)
 
     height = len(problem)
+    width = len(problem[0])
     num_cand = len(candidates)
 
     print("候補が{}個あります".format(num_cand))
     for i in range(num_cand):
-        print("{}. {}".format(i+1, candidates[i]))
+        print("{}. {}".format(i+1, candidates[i][0]))
     print("")
 
     if num_cand < height:
         print("候補が少なすぎます")
         return []
 
-    ans = []
+    def _check_gray(combination) -> bool:
+        gray_char = None
+        for pos in combination:
+            if candidates[pos][1] is not None:
+                if gray_char is None:
+                    gray_char = candidates[pos][1]
+                elif candidates[pos][1] != gray_char:
+                    return False
+        return True
+
+    def _get_gray_char(combination) -> str:
+        for pos in combination:
+            if candidates[pos][1] is not None:
+                return candidates[pos][1]
+        return None
+
+    def _compare_problem(board: list, gray_char: str) -> bool:
+        for row in range(width):
+            board_slice = []
+            problem_slice = []
+            for column in range(height):
+                board_slice.append(board[column][row])
+                if problem[column][row] == GRAY and gray_char is not None:
+                    problem_slice.append(gray_char)
+                else:
+                    problem_slice.append(problem[column][row])
+            if sorted(board_slice) != sorted(problem_slice):
+                return False
+        return True
+
+    answers = []
     for combination in itertools.combinations(range(num_cand), height):
-        board = [candidates[i] for i in combination]
-        if sort_selectwords(board) == sorted_problem:
-            ans.append(board)
+        if not _check_gray(combination):
+            continue
 
-    return ans
+        board = [candidates[i][0] for i in combination]
+        gray_char = _get_gray_char(combination)
+        if _compare_problem(board, gray_char):
+            answers.append(board)
 
-
-def sort_selectwords(selectwords: list):
-    listed = [list(i) for i in selectwords]
-    return sort(listed, axis=0).tolist()
+    return answers
 
 
 def show_selectwords(selectwords: list) -> None:
